a3e6cac33de940d052e498ce972b764c57790b91
RiyaRani23 <riyarani1404@gmail.com>
2025-09-04 16:05:17 +0530

    Promises

---
+++ Revision/Promise.js
@@ -0,0 +1,15 @@
+const promise = new Promise((resolve, reject) => {
+    if(true) {
+        resolve ('Stuff Worked');
+    } else {
+        reject ('Error , it broke')
+    }
+})
+
+promise
+  .then(result => result + '!')
+  .then(result2 => result2 + '?')
+  .catch(() => console.log('Error'))
+  .then (result3 => {
+    console.log(result3 + '!');
+  })
