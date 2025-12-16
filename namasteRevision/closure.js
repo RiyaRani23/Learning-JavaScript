f4f43848f9d67b0adcd13b16f49689d58524cd67
RiyaRani23 <riyarani1404@gmail.com>
2025-12-16 13:14:14 +0530

    Closure behaviour

---
+++ namasteRevision/closure.js
@@ -0,0 +1,16 @@
+function x() {
+    var a = 10;
+    function y() {
+        console.log(a);
+    }
+    
+    y();
+    a = 100;
+}
+x();// Output: 100 -> reason: Closure captures variable by reference, not by value.
+// Explanation:
+// 1. The function `x` is defined and called.
+// 2. Inside `x`, a variable `a` is initialized to 10.
+// 3. The nested function `y` is defined, which logs the value of `a`.
+// 4. Before calling `y`, the value of `a` is changed to 100.
+// 5. When `y` is called, it accesses the current value of `a`, which is now 100, due to closure behavior in JavaScript.
