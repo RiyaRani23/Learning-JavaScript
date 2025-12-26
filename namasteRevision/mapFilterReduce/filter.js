08e099fa42a8592deed535976fe1d571b5dd8256
RiyaRani23 <riyarani1404@gmail.com>
2025-12-26 10:38:32 +0530

    filter function

---
+++ namasteRevision/mapFilterReduce/filter.js
@@ -0,0 +1,6 @@
+const arr = [1,4,3,5,3,57,7];
+
+// Filter method creates a new array with all elements that pass the test implemented by the provided function.
+const output = arr.filter((x) => {
+     return x>4;
+})
